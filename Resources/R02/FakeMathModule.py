a01c280c1084c860df89651c32a1eba401e869c0
rugbyprof <terry.griffin@msutexas.edu>
2020-07-08 06:46:23 -0500

    lesson 17

---
+++ Resources/R02/FakeMathModule.py
@@ -0,0 +1,18 @@
+# Silly advanced math module
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def main():
+    print(add(34,34))
+    print(sub(33,22))
+    print(mul(4,5))
+
+if __name__=='__main__':
+    main()
---
+++ Resources/R02/PyIntro_17.py
@@ -0,0 +1,14 @@
+import sys
+import os
+#from FakeMathModule import add
+import FakeMathModule
+#from FakeMathModule import *
+
+# PyIntro Lesson 17
+# touch of modules (barely)
+# really name == main
+
+#print(FakeMathModule.add(8,9))
+print(FakeMathModule.add(8,9))
+
+print(FakeMathModule.sub(88,33))
